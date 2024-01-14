a9afbea2a0cef974e1a416e7236c83d6a086c5f0
Mohsin-Alaini <mohsin.alaini2020@gmail.com>
2024-01-14 23:30:05 +0300

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -9,5 +9,5 @@ def view2(request,id):
     return HttpResponse('view2')
 def view3(request,id,name):
     return HttpResponse('view3')
-def view4(request):
-    return HttpResponse('view4')
+# def view4(request):
+#     return HttpResponse('view4')
