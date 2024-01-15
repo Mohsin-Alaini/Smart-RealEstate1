7869af05f83ddffbaefafcfc401aba6fb9a66010
Mohsin-Alaini <mohsin.alaini2020@gmail.com>
2024-01-15 20:45:25 +0300

    .

--- proj1/settings.py
+++ proj1/settings.py
@@ -82,6 +82,19 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'djangoco',
+#         'USER':'postgres',
+#         'PASSWORD':'post@123',
+#         'HOST':'127.0.0.1',
+#         'PORT':5432
+        
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
